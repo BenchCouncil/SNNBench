5ae7b4ce05709e954f54c521e27e478a477aff53
feitang0 <iamtangfei@gmail.com>
2022-10-20 20:36:25 +0800

    use tee instead

--- workloads/speech/run_profiler.sh
+++ workloads/speech/run_profiler.sh
@@ -1,7 +1,8 @@
-for device in cpu cuda
+# for device in cpu cuda
+for device in cpu
 do
 	for model in lif lsnn lstm
 	do
-		./run_in_docker.sh python speech_profile.py --device ${device} --model ${model} --log "./${device}_${model}.pt.trace.json"
+		./run_in_docker.sh python speech_profile.py --device ${device} --model ${model} | tee $(pwd)/../logs/speech-inference-${model}-${device}.txt
 	done
 done
